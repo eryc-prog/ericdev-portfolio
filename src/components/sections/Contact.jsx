84c0782503107eca090eb60a07916b1c1ca19487
Eric Encarnacion <encarnacionerc@gmail.com>
2025-09-03 22:14:04 +0800

    fix: make the form responsive

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -32,10 +32,9 @@ export const Contact = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="px-4 w-150 ">
-          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-            {" "}
-            Get in Touch{" "}
+        <div className="w-full max-w-md mx-auto py-8 px-2 sm:px-4">
+          <h2 className="text-2xl sm:text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            Get in Touch
           </h2>
           <form className="space-y-6" onSubmit={handleSubmit}>
             <div className="relative">
